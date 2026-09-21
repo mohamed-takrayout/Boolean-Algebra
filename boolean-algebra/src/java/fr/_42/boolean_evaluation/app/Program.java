c124928240be1ff785d068d454fac42257f7a07d
Mohamed Takrayout <takrayoutm@gmail.com>
2026-09-22 00:12:52 +0100

    Update : adding eval formula to evaluate the result of a sequence of operation of | & ! ^ > and = in reverse polish notation format

---
+++ boolean-algebra/src/java/fr/_42/boolean_evaluation/app/Program.java
@@ -0,0 +1,26 @@
+package fr._42.boolean_evaluation.app;
+
+import fr._42.boolean_evaluation.logic.EvalFormula;
+
+public class Program {
+
+    private static void printEvalFormula(String str) {
+        try {
+            boolean result = EvalFormula.evalFormula(str);
+            System.out.printf("[%s] output result is [%b]", str, result);
+        } catch (IllegalArgumentException e) {
+            System.err.print(e.getMessage());
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        printEvalFormula("10&");
+        printEvalFormula("10|");
+        printEvalFormula("10|1&");
+        printEvalFormula("101|&");
+        printEvalFormula("11>");
+        printEvalFormula("11>>");
+        printEvalFormula("1011||=");
+    }
+}
---
+++ boolean-algebra/src/java/fr/_42/boolean_evaluation/logic/EvalFormula.java
@@ -0,0 +1,73 @@
+package fr._42.boolean_evaluation.logic;
+
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.function.IntBinaryOperator;
+
+public class EvalFormula {
+
+    private final static Map<Character, IntBinaryOperator> OPERATORS = Map.of(
+            '!', (a, b) -> a == 1 ? 0 : 1,
+            '&', (a, b) -> a & b,
+            '|', (a, b) -> a | b,
+            '^', (a, b) -> a ^ b,
+            '>', (a, b) -> a == b ? 1 : 0,
+            '=', (a, b) -> a == b ? 1 : 0
+    );
+
+    private EvalFormula() {
+    }
+
+    private static boolean isOperator(char c) {
+        return switch (c) {
+            case '!' ->
+                true;
+            case '&' ->
+                true;
+            case '|' ->
+                true;
+            case '^' ->
+                true;
+            case '>' ->
+                true;
+            case '=' ->
+                true;
+            default ->
+                false;
+        };
+    }
+
+    public static boolean evalFormula(String str) throws IllegalArgumentException {
+        LinkedList<Boolean> LINKED_LIST_OPERATIONS = new LinkedList<>();
+        boolean first;
+        boolean second;
+        for (char c : str.toCharArray()) {
+            if (isOperator(c)) {
+                if (LINKED_LIST_OPERATIONS.size() < 2 && c != '!') {
+                    throw new IllegalArgumentException("Entered non valid sequence of operations : [" + str + "]");
+                }
+                first = LINKED_LIST_OPERATIONS.remove();
+                if (c == '!') {
+                    LINKED_LIST_OPERATIONS.addFirst(!first);
+                    continue;
+                }
+                second = LINKED_LIST_OPERATIONS.remove();
+                int newValue = OPERATORS.get(c).applyAsInt(first ? 1 : 0, second ? 1 : 0);
+                LINKED_LIST_OPERATIONS.addFirst(newValue == 1);
+            } else {
+                if (c == '1') {
+                    LINKED_LIST_OPERATIONS.addLast(true);
+                } else if (c == '0') {
+                    LINKED_LIST_OPERATIONS.addLast(false);
+                } else {
+                    throw new IllegalArgumentException("Entered non valid sequence of operations : [" + str + "]");
+                }
+            }
+        }
+        if (LINKED_LIST_OPERATIONS.size() != 1) {
+            throw new IllegalArgumentException("Entered non valid sequence of operations : [" + str + "]");
+        }
+        return LINKED_LIST_OPERATIONS.remove();
+
+    }
+}
